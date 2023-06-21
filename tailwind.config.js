a53fad78c928b4c81eb62b76576839946fed6d98
Dani Matuko <matuko305@gmail.com>
2023-06-21 21:18:10 +0300

    add general styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: "#FF176B",
+        gold: "#D1AD78",
+      },
+    },
   },
   plugins: [],
 };
