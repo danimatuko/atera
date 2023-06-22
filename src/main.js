0721d31fd1e7700b8c8ca567446b626b435cbcb7
Dani Matuko <matuko305@gmail.com>
2023-06-22 15:48:09 +0300

    run script after page load

--- src/main.js
+++ src/main.js
@@ -1,38 +1,41 @@
-const slider = document.querySelector(".slider");
-const slides = document.querySelectorAll(".slides");
-const dots = document.querySelectorAll(".dots button");
+window.onload = function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slides");
+  const dots = document.querySelectorAll(".dots button");
 
-let counter = 0;
+  let counter = 0;
 
-let size = slides[0].clientWidth;
+  let size = slides[0].clientWidth;
 
-function initSlider() {
-  dots.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-      counter = index;
-      slide();
-      resetDots();
-      dot.classList.add("active");
+  function initSlider() {
+    dots.forEach((dot, index) => {
+      dot.addEventListener("click", () => {
+        counter = index;
+        slide();
+        resetDots();
+        dot.classList.add("active");
+      });
     });
-  });
-}
+  }
 
-function slide() {
-  slider.style.transform = `translateX(${-size * counter}px)`;
-}
+  function slide() {
+    slider.style.transform = `translateX(${-size * counter}px)`;
+  }
 
-function resetDots() {
-  dots.forEach((dot) => {
-    dot.classList.remove("active");
-  });
-}
+  function resetDots() {
+    dots.forEach((dot) => {
+      dot.classList.remove("active");
+    });
+  }
 
-function handleResize() {
+  function handleResize() {
+    size = slides[0].clientWidth;
+    slide();
+  }
+
+  window.addEventListener("resize", handleResize);
+
+  initSlider();
   size = slides[0].clientWidth;
-  slide();
-}
-
-window.addEventListener("resize", handleResize);
-
-initSlider();
-slide(); // Call slide() after initSlider() to ensure the initial slide is set correctly
+  slide(); // Call slide() after initSlider() to ensure the initial slide is set correctly
+};
