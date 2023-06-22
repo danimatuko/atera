506e498d383de63cfd321f5c26dc1c42e9ed9a18
Dani Matuko <matuko305@gmail.com>
2023-06-22 09:44:44 +0300

    add finish mobile slider

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@ const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slides");
 const dots = document.querySelectorAll(".dots button");
 
-console.log(slider, slides);
-
 const size = slides[0].clientWidth;
 
-console.log(size);
-const counter = 3;
+const counter = 0;
 
 slider.style.transform = "translateX(" + -size * counter + "px)";
 
 dots.forEach((dot, index) => {
   dot.addEventListener("click", (e) => {
-    console.log(e.target, index);
     slider.style.transform = "translateX(" + -size * index + "px)";
+    resetDots();
+    dot.classList.add("active");
   });
 });
+
+const resetDots = () => {
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+};
